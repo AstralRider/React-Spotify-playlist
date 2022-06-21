7d903b9b87bd33c76cce88b427e21b2588c2117c
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-06-21 17:18:31 +1000

    base file structure and components

--- spotify-api/src/Components/App/App.js
+++ spotify-api/src/Components/App/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
 import React from "react";
+import { SearchBar } from "../SearchBar/SearchBar";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>
-          Ja<span class="highlight">mmm</span>ing
+          Ja<span className="highlight">mmm</span>ing
         </h1>
-        <div class="App">
-          {/* <SearchBar /> */}
-          <div class="App-playlist">
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
             {/* <SearchResults /> */}
             {/* <Playlist /> */}
           </div>
---
+++ spotify-api/src/Components/Track/Track.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export class Track extends React.Component {
+  renderAction() {
+    if (this.props.isRemoval) {
+      return <button className="Track-action">-</button>;
+    } else {
+      return <button className="Track-action">+</button>;
+    }
+  }
+
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>Track name</h3>
+          <p>track artist | track album</p>
+        </div>
+        {this.renderAction()}
+      </div>
+    );
+  }
+}
