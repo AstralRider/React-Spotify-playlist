b00cae84aa1ffe58e7b1070e7d81b696730db21d
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-06-21 15:34:37 +1000

    another commit

---
+++ spotify-api/src/Components/App/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span class="highlight">mmm</span>ing
+        </h1>
+        <div class="App">
+          {/* <SearchBar /> */}
+          <div class="App-playlist">
+            {/* <SearchResults /> */}
+            {/* <Playlist /> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
