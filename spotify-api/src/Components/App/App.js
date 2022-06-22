f3eb034d43f3837d9642660b72dda9e477e5846f
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-06-22 20:21:46 +1000

    uris

--- spotify-api/src/Components/App/App.js
+++ spotify-api/src/Components/App/App.js
@@ -28,6 +28,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -48,6 +49,10 @@ class App extends React.Component {
 
   updatePlaylistName(name) {
     this.setState({ playlistName: name });
+  }
+
+  savePlaylist() {
+    let trackUris = this.state.playlistTracks.map((track) => track.uri);
   }
 
   render() {
