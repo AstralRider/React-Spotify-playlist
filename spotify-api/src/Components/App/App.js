79ff874415b3d41cce26de048ff0cb48718674a0
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-06-23 19:51:21 +1000

    connected to spotify api

--- spotify-api/src/Components/App/App.js
+++ spotify-api/src/Components/App/App.js
@@ -3,26 +3,16 @@ import React from "react";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { SearchResults } from "../SearchResults/SearchResults";
 import { Playlist } from "../Playlist/Playlist";
+import Spotify from "../../util/Spotify";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      searchResults: [
-        { name: "name1", artist: "artist1", album: "album1", id: 1 },
-        { name: "name2", artist: "artist2", album: "album2", id: 2 },
-        { name: "name3", artist: "artist3", album: "album3", id: 3 },
-      ],
+      searchResults: [],
       playlistName: "defaultName",
-      playlistTracks: [
-        {
-          name: "playlistname1",
-          artist: "playlistartist1",
-          album: "playlistalbum1",
-          id: 1,
-        },
-      ],
+      playlistTracks: [],
     };
 
     this.addTrack = this.addTrack.bind(this);
@@ -54,10 +44,18 @@ class App extends React.Component {
 
   savePlaylist() {
     let trackUris = this.state.playlistTracks.map((track) => track.uri);
+    Spotify.savePlayList(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: "New Playlist",
+        playlistTracks: [],
+      });
+    });
   }
 
   search(term) {
-    console.log(term);
+    Spotify.search(term).then((searchResults) => {
+      this.setState({ searchResults: searchResults });
+    });
   }
 
   render() {
